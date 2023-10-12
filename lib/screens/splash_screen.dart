7b1e8e32fe70f529ee0c520eb1e8e8e91ec5fe93
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2023-10-12 17:37:37 +0530

    splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: SvgPicture.asset('assets/logoipsum-255 1.svg')),
+    );
+  }
+}
